5790f8cee929ce06dd8db74317613191e9f2862f
Stella Camargo <stellacv2001@hotmail.com>
2024-06-27 00:05:32 -0300

    Ajustando pastas

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function toggleMode() {
   // substiruir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-ste-light.jpg")
+    img.setAttribute("src", "../assets/avatar-ste-light.jpg")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar-ste-night.png")
+    img.setAttribute("src", "../assets/avatar-ste-night.png")
   }
 }
